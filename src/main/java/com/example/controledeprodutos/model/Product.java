59c127de64895e5bb863e88a4b408eaca86fa131
Felipe Alves <felipe.daconceicao@jala.university>
2024-04-06 12:31:41 -0300

    feat[Product]: Crud do produto em modo demo

---
+++ src/main/java/com/example/controledeprodutos/model/Product.java
@@ -0,0 +1,58 @@
+package com.example.controledeprodutos.model;
+
+public class Product {
+    private Integer id;
+    private String name;
+    private Integer amount;
+    private Double price;
+    private String observation;
+    private String description;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getObservation() {
+        return observation;
+    }
+
+    public void setObservation(String observation) {
+        this.observation = observation;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
